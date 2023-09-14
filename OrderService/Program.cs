49e15bdff3e6ad8ca7d9302e28f62d4ea4d906c6
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2023-09-14 14:03:46 +0530

    retry multiple times without waiting.

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,13 +1,43 @@
 using Microsoft.Extensions.Options;
 using OrderService.Clients;
 using OrderService.Options;
+using Polly;
+using Polly.Extensions.Http;
+using Serilog;
+
+
+Log.Logger = new LoggerConfiguration().Enrich.FromLogContext()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up Inventory Service.....");
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog(
+    (hostBuilderContext, services, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostBuilderContext.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
 
+// Add services to the container.
 var services = builder.Services;
 
+
+// Create the retry policy we want
+var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError() // HttpRequestException, 5XX and 408  
+    .RetryAsync(4);
+
+// Register the InventoryClient with Polly policies
+services.AddHttpClient<IInventoryClient, InventoryClient>().ConfigureHttpClient(
+    (serviceProvider, httpClient) =>
+    {
+        var httpClientOptions = serviceProvider.GetRequiredService<InventoryClientOptions>();
+
+        httpClient.BaseAddress = httpClientOptions.BaseAddress;
+        httpClient.Timeout = httpClientOptions.Timeout;
+        
+    }).AddPolicyHandler(retryPolicy);
 
 services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,15 +52,6 @@ services.AddOptions<InventoryClientOptions>()
 services.AddSingleton(
     resolver => resolver.GetRequiredService<IOptions<InventoryClientOptions>>()
         .Value);
-
-services.AddHttpClient<IInventoryClient, InventoryClient>().ConfigureHttpClient(
-    (serviceProvider, httpClient) =>
-    {
-        var httpClientOptions = serviceProvider.GetRequiredService<InventoryClientOptions>();
-
-        httpClient.BaseAddress = httpClientOptions.BaseAddress;
-        httpClient.Timeout = httpClientOptions.Timeout;
-    });
 
 var app = builder.Build();
 
